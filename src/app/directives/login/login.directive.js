acdb01ebdb08a9b6e52e370bf1669c1f7e34251b
BeaMorim <biadmorim@gmail.com>
2017-12-01 15:04:44 -0200

    Iniciando diretiva login

---
+++ src/app/directives/login/login.directive.js
@@ -0,0 +1,10 @@
+myApp
+    .controller("loginController", function () {
+
+    })
+
+    .directive("login", function() {
+        return {
+            templateUrl: "app/directives/login/login.template.html"
+        }
+    })
