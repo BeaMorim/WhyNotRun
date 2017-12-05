548eba1ba874925a00782cd79bab4d53c515b079
BeaMorim <biadmorim@gmail.com>
2017-12-05 14:01:25 -0200

    Comportamento exibição comentarios

--- src/app/directives/post/post.directive.js
+++ src/app/directives/post/post.directive.js
@@ -1,7 +1,16 @@
-myApp.controller("postController", [function() {
+myApp.controller("postController", ["$scope", function($scope) {
+
+    $scope.commentsAreaStatus = function() {
+        if($scope.commentsAreaActive)
+            $scope.commentsAreaActive = false;
+        else
+            $scope.commentsAreaActive = true;
+    }
 }])
+
 .directive("post", function() {
     return {
-        templateUrl:"app/directives/post/post.template.html"
+        templateUrl:"app/directives/post/post.template.html",
+        controller: "postController"
     }
 })
