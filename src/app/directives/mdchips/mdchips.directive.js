3a373c38ba58d87beb53de8bbd975e564f1238e9
BeaMorim <biadmorim@gmail.com>
2017-12-11 12:25:37 -0200

    Criação do autocomplete

--- src/app/directives/mdchips/mdchips.directive.js
+++ src/app/directives/mdchips/mdchips.directive.js
@@ -5,6 +5,21 @@ myApp
             restrict: 'E',
             require: 'mdChips',
             link: function ($scope, element, attributes, mdChipsCtrl) {
+
+                mdChipsCtrl.prototype.appendChip = function(newChip) {
+                    
+                      if (typeof (newChip) == "string") newChip = {"name": newChip, "_lowername": angular.lowercase(newChip)};
+                      if(newChip._lowername == undefined)newChip._lowername = angular.lowercase(newChip.name);
+                    
+                      for(var i = 0 ; i < this.items.length;i++){
+                        if(this.items[i]._lowername == newChip._lowername) return;
+                      }
+                      this.items.push(newChip);
+                      return;
+                    
+                    
+                    };
+                    
                 mdChipsCtrl.onInputBlur = function () {
                     this.inputHasFocus = false;
 
@@ -19,6 +34,17 @@ myApp
                         element[0].parentElement.className = "input-group";
                     }
                 };
+
+                window.addEventListener('click', function(event) {
+                    while (mouseUpActions.length > 0) {
+                      var action = mouseUpActions.splice(0, 1)[0];
+                      $timeout(function() {
+                        $timeout(action);
+                      });
+                    }
+                }, false);
+
+                
 
                 
             }
