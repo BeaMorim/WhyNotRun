b204441529c588ee575ab285b0107881f2917763
BeaMorim <biadmorim@gmail.com>
2017-12-22 10:48:51 -0200

    Fixing rankingController

--- src/app/directives/ranking/ranking.directive.js
+++ src/app/directives/ranking/ranking.directive.js
@@ -1,4 +1,16 @@
 myApp
+    .controller("rankingController", function($scope, Ranking){
+
+        $scope.loadRanking = function(){
+            Ranking.getRanking()
+                .then(function(data) {
+                    $scope.ranking = data.data;
+                });
+        }
+
+        $scope.loadRanking();
+    })
+
     .directive("ranking", function() {
         return {
             templateUrl: "app/directives/ranking/ranking.template.html",
--- src/app/modules/ranking/ranking.controller.js
+++ src/app/modules/ranking/ranking.controller.js
@@ -1,10 +1,10 @@
-ranking.controller("rankingController", ["Ranking", "$http", "$scope", function(Ranking, $http, $scope, $routeParams, contato) {
-    var loadRanking = function() {
-        Ranking.getRanking().then(function(data) {
-            $scope.ranking = data.data;
-        });
-    };
+// ranking.controller("rankingController", ["Ranking", "$http", "$scope", function(Ranking, $http, $scope, $routeParams, contato) {
+//     var loadRanking = function() {
+//         Ranking.getRanking().then(function(data) {
+//             $scope.ranking = data.data;
+//         });
+//     };
 
     
-    loadRanking();
-}]);
+//     loadRanking();
+// }]);
